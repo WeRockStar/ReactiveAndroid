f8f92ce4cb5cceeb4c61c861214cfc5e427e5cc0
WeRockStar <kotchaphan.m@gmail.com>
2017-05-09 15:38:13 +0700

    Add gradle wrapper

--- app/src/main/java/com/werockstar/reactiveandroid/api/RxApi.java
+++ app/src/main/java/com/werockstar/reactiveandroid/api/RxApi.java
@@ -5,7 +5,6 @@ import com.werockstar.reactiveandroid.model.GithubUserResponse;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
-import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface RxApi {
